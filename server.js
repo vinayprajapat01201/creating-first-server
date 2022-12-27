363ce1a43fd4f57f3cff0a43589afa1d0e173fee
vinayprajapat01201 <vinayprajapat01201@gmail.com>
2022-12-28 01:44:18 +0530

    Completed code of first nodejs sever

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = 8081;
+
+http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>Hello this is from my server</h1>");
+    response.end();
+
+}).listen(port, () => {
+    console.log(`Nodejs server started on port ${port}`);
+});
+
+// http://Localhost:8081
